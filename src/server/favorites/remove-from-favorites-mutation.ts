f63cff5b1a8152aaaa589b3948a46bd0c50b43a0
carl <carlos.acitores@colba.es>
2026-02-22 13:21:55 +0100

    resolve todos

--- src/server/favorites/remove-from-favorites-mutation.ts
+++ src/server/favorites/remove-from-favorites-mutation.ts
@@ -3,12 +3,21 @@ import { protectedProcedure } from "../infrastructure/trpc/trpc";
 import { buildFavoriteAlbumId, favoritesAlbumsTable } from "./favoritesAlbums.schema";
 import { eq } from "drizzle-orm";
 
-// TODO - check if if album is already in favorites, if it is not finish process, if it is execute delete
 export const removeAlbumFromFavoritesMutation = protectedProcedure
   .input(z.object({ albumId: z.string() }))
   .mutation(async (
     { input: { albumId }, ctx: { db, userId } }
-  ) => db
-    .delete(favoritesAlbumsTable)
-    .where(
-      eq(favoritesAlbumsTable.id, buildFavoriteAlbumId(albumId, userId))));
+  ) => {
+    const existing = await db
+      .select()
+      .from(favoritesAlbumsTable)
+      .where(
+        eq(favoritesAlbumsTable.id, buildFavoriteAlbumId(albumId, userId)));
+    if (existing.length === 0) return;
+
+    await db
+      .delete(favoritesAlbumsTable)
+      .where(
+        eq(favoritesAlbumsTable.id, buildFavoriteAlbumId(albumId, userId)),
+      );
+  });
--- src/server/pending/remove-from-pending-mutatin.ts
+++ src/server/pending/remove-from-pending-mutatin.ts
@@ -1,14 +1,22 @@
 import z from "zod";
 import { protectedProcedure } from "../infrastructure/trpc/trpc";
 import { buildPendingAlbumId, pendingAlbumsTable } from "./pendingAlbums.schema";
-import { and, eq } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 
-// TODO - check if if album is already in pending, if it is not finish process, if it is execute delete
 export const removeAlbumFromPendingMutation = protectedProcedure
   .input(z.object({ albumId: z.string() }))
-  .mutation(
-    async ({ input: { albumId }, ctx: { db, userId } }
-  ) => db
-    .delete(pendingAlbumsTable)
-    .where(
-      eq(pendingAlbumsTable.id, buildPendingAlbumId(albumId, userId))));
+  .mutation(async (
+    { input: { albumId }, ctx: { db, userId } }
+  ) => {
+    const existing = await db
+      .select()
+      .from(pendingAlbumsTable)
+      .where(
+        eq(pendingAlbumsTable.id, buildPendingAlbumId(albumId, userId)));
+    if (existing.length === 0) return;
+
+    await db
+      .delete(pendingAlbumsTable)
+      .where(
+        eq(pendingAlbumsTable.id, buildPendingAlbumId(albumId, userId)));
+  });
