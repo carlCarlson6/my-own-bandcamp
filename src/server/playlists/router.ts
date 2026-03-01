f45c36d6e718427b9a433fce5d5c021624ba1023
carl <carlos.acitores@colba.es>
2026-03-01 20:36:52 +0100

    feat: add updatePlaylistName mutation for renaming playlists

--- src/server/playlists/router.ts
+++ src/server/playlists/router.ts
@@ -4,11 +4,13 @@ import { getPlaylistQuery } from "./get-playlist.query";
 import { getPlaylistsResumeQuery } from "./get-playlists-resume-query";
 import { ramoveFromPlaylistMutation } from "./ramove-from-playlist-mutation";
 import { saveToPlaylistMutation } from "./save-to-playlist-mutation";
+import { updatePlaylistNameMutation } from "./update-playlist-name-mutation";
 
 export const playlistsRouter = createTRPCRouter({
-  create:    createPlaylistMutation,
-  getResume: getPlaylistsResumeQuery,
-  get:       getPlaylistQuery,
-  save:      saveToPlaylistMutation,
-  remove:    ramoveFromPlaylistMutation,
+  create:     createPlaylistMutation,
+  getResume:  getPlaylistsResumeQuery,
+  get:        getPlaylistQuery,
+  save:       saveToPlaylistMutation,
+  remove:     ramoveFromPlaylistMutation,
+  updateName: updatePlaylistNameMutation
 });
---
+++ src/server/playlists/update-playlist-name-mutation.ts
@@ -0,0 +1,35 @@
+import z from "zod";
+import { protectedProcedure } from "../infrastructure/trpc";
+import { userPlaylistsTable } from "./playlists.schema";
+import { and, eq } from "drizzle-orm";
+
+export const updatePlaylistNameMutation = protectedProcedure
+  .input(z.object({
+    playlistId: z.string(),
+    newName: z.string().min(1).max(1000)
+  }))
+  .mutation(async ({ ctx: { db, userId }, input: { playlistId, newName }, }) => {
+    const playlist = (await db
+      .select({
+        id: userPlaylistsTable.id
+      })
+      .from(userPlaylistsTable)
+      .where(
+        and(
+          eq(userPlaylistsTable.id, playlistId),
+          eq(userPlaylistsTable.userId, userId)
+        )
+      ))
+      .at(0);
+    if (!playlist) {
+      throw new Error("Playlist not found");
+    }
+
+    const result = await db
+      .update(userPlaylistsTable)
+      .set({ name: newName })
+      .where(eq(userPlaylistsTable.id, playlistId));
+    if (result.count !== 1) {
+      throw new Error("Failed to update playlist name");
+    }
+  });
